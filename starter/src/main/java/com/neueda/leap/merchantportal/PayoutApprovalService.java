469898a111d7986e36774f013a48dcf9a7b2d267
Upasana Patel <Upasana Patel>
2026-08-27 19:53:17 +0000

    Change payout approval

--- starter/src/main/java/com/neueda/leap/merchantportal/PayoutApprovalService.java
+++ starter/src/main/java/com/neueda/leap/merchantportal/PayoutApprovalService.java
@@ -11,7 +11,11 @@ public class PayoutApprovalService {
     public void approve(Long payoutId, Long approvingUserId) {
         PayoutRequest payout = payoutRepository.findById(payoutId)
                 .orElseThrow(() -> new RuntimeException("Payout not found"));
-
+// preventing self approval by segregating duties
+	if (payout.getRequestedByUserID().equals(approvingUserID)){
+		throw new IllegalException(
+			"A payout cannot be approved by the same user who requested it");
+	}
         payout.setApprovalStatus("APPROVED");
         payout.setApprovedByUserId(approvingUserId);
         payoutRepository.save(payout);
